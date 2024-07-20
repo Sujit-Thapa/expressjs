55cea0bb748106c9c5e62abb0d333dc84ca7c2f8
Sujit-Thapa <sujitthapa.professiona@gmaill.com>
2024-07-20 14:03:26 +0545

    express

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-const _ = require('lodash')
+const express = require('express')
+const app = express()
 
-const items =[ 1, [2, [3, [4]]]]
+const { products } = require('./data')
 
-const newItems = _.flattenDeep(items)
-console.log(newItems)
-console.log('hello people')
+app.get(('/'),(req,res)=>
+{
+    res.send('<h1>Home page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products/:productID',(req,res)=>
+    {
+        //onsole.log(req)
+       // console.log(req.params)
+       const {productID}= req.params
+      const singleproduct = products.find(
+        (product)=> product.id == Number(productID) )
+if(!singleproduct){
+    return res.status(404).send('The product is not available')
+}
+   res.json(singleproduct)
+    })
+    
+    app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>
+        {
+         
+       res.send('hello world')
+   
+        })
+        
+
+app.listen(5000,() =>
+{
+    console.log('listening on port 5000.....');
+})
---
+++ express/1-intro.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app= express()
+
+app.get('/home',(req,res)=>{
+    res.status(200).send('Home Page')
+    console.log('user hit the resource')
+  
+
+})
+
+app.get('/about',(req,res)=>{
+
+    res.status(200).send('About page')
+}
+)
+
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>resource not found</h1>')
+})
+
+app.listen(5000,() => {
+    console.log('server is listening at port 5000.....')
+    
+})
+
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
