5aa9e701de0fbb3b269caba4bf00f7be41cbb6ab
Sujit-Thapa <sujitthapa.professiona@gmaill.com>
2024-10-21 16:21:52 +0545

    updated the logger

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const express = require('express')
 const app= express()
+const logger= require('./logger')
+const authorize = require('./authorize')
 //req => middleware => res
 
-const logger = (req, res, next)=> {
-    const method = req.method
-    const url =req.url
-    const time = new Date().getFullYear()
-    console.log(method, url ,time)
-    //res.send('test')
-    next()
-
-
-}
-app.get('/',logger,(req,res) => {
+/*app.get('/',logger,(req,res) => {
     
     res.send('home')
 })
@@ -24,3 +16,55 @@ app.get('/about',logger, (req,res) => {
 app.listen(5000,() => {
     console.log('server is listening on 5000')
 })
+
+//to use the app without using logger functuion can be done by 
+
+app.use(logger)
+app.get('/',(req,res) => {
+    
+    res.send('home')
+})
+
+app.get('/about', (req,res) => {
+    res.send('about')
+})
+
+app.listen(5000,() => {
+    console.log('server is listening on 5000')
+})
+
+//to use the middleware so that the every finction below it can get the middleware 
+we use 
+
+app.use('api',logger) //this uses and functions so as that the web startin with api automatically uses the middlewre
+app.get('/',(req,res) => {
+    
+    res.send('home')
+})
+
+app.get('/about', (req,res) => {
+    res.send('about')
+})
+
+app.listen(5000,() => {
+    console.log('server is listening on 5000')
+})*/
+
+
+//using the middleware authorize
+
+
+app.use(logger,authorize)
+app.get('/',(req,res) => {
+    
+    res.send('home')
+})
+
+app.get('/about', (req,res) => {
+    res.send('about')
+})
+
+app.listen(5000,() => {
+    console.log('server is listening on 5000')
+})
+
